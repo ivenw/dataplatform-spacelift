4d90178a2db36d139d410cc5cdb922d3d2125b6f
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-03-21 17:12:41 +0100

    context

--- spacelift/main.tf
+++ spacelift/main.tf
@@ -1,11 +1,8 @@
-import {
-  to = spacelift_stack.spacelift
-  id = "spacelift"
-}
+data "spacelift_current_space" "this" {}
 
 resource "spacelift_stack" "spacelift" {
   name     = "spacelift"
-  space_id = "root"
+  space_id = data.spacelift_current_space.this.id
 
   repository   = "dataplatform-spacelift"
   branch       = "main"
@@ -17,7 +14,34 @@ resource "spacelift_stack" "spacelift" {
 
 resource "spacelift_space" "dataplatform" {
   name            = "dataplatform"
-  parent_space_id = "root"
+  parent_space_id = data.spacelift_current_space.this.id
+  description     = "All stacks for the data platform"
+}
+
+resource "spacelift_context" "global" {
+  name        = "global"
+  space_id    = spacelift_space.dataplatform.id
+  description = "Global context"
+  labels      = ["autoattach:global"]
+}
+resource "spacelift_environment_variable" "location" {
+  context_id = spacelift_context.global.id
+  name       = "TF_VAR_location"
+  value      = "westeurope"
+  write_only = false
+}
+
+resource "spacelift_context" "environment_dev" {
+  name        = "environment-dev"
+  space_id    = spacelift_space.dataplatform.id
+  description = "Development environment context"
+  labels      = ["autoattach:dev"]
+}
+resource "spacelift_environment_variable" "environment_dev" {
+  context_id = spacelift_context.environment_dev.id
+  name       = "TF_VAR_environment_slug"
+  value      = "dev"
+  write_only = false
 }
 
 resource "spacelift_stack" "databricks_dev" {
@@ -27,4 +51,6 @@ resource "spacelift_stack" "databricks_dev" {
   repository   = "dataplatform-spacelift"
   branch       = "main"
   project_root = "stacks/databricks-workspace"
+
+  labels = ["global", "dev"]
 }
