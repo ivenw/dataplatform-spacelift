c6a80735f17cea398300e2f53a6f4b7dfd8d6233
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-03-21 16:49:20 +0100

    bring in admin stack

--- spacelift/main.tf
+++ spacelift/main.tf
@@ -1,5 +1,25 @@
+resource "spacelift_stack" "spacelift" {
+  name     = "spacelift"
+  space_id = "root"
+
+  repository   = "dataplatform-spacelift"
+  branch       = "main"
+  project_root = "spacelift"
+
+  administrative = true
+  auto_apply     = true
+}
+
+resource "spacelift_space" "dataplatform" {
+  name            = "dataplatform"
+  parent_space_id = "root"
+}
+
 resource "spacelift_stack" "databricks_dev" {
-  name       = "databricks-dev"
-  repository = "dataplatform-spacelift"
-  branch     = "main"
+  name     = "databricks-dev"
+  space_id = spacelift_space.dataplatform.id
+
+  repository   = "dataplatform-spacelift"
+  branch       = "main"
+  project_root = "stacks/databricks-workspace"
 }
